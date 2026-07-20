aac5777c097cda674a89ff513536020ca3129d19
James <aslan.jnn@gmail.com>
2026-07-20 21:36:12 +1000

    Add flags to clean-repo.sh

--- clean-repo.sh
+++ clean-repo.sh
@@ -3,11 +3,13 @@ cd "${0:a:h}"
 VSCODE_REPO=work/vscode
 
 function show_help() {
-    echo "Usage: ${0:a:t} [deep]"
+    echo "Usage: ${0:a:t} [--deep] [-y]"
     echo
-    echo "Revert all modifications to the 'vscode' repository and also clean untracked files."
-    echo "If 'deep' is specified, additionally clean files specified in '.gitignore'. These files includes build artefacts and 'node_modules'."
-    echo "It is normally not necessary to perform a deep clean."
+    echo "Reset the vscode repository to a pristine state of the base commit."
+    echo
+    echo "Options:"
+    echo "  --deep  Also remove files ignored by .gitignore (build artefacts, node_modules)"
+    echo "  -y      Skip the confirmation prompt"
     exit 1
 }
 
@@ -17,15 +19,13 @@ then
     exit
 fi
 
-if [[ $# -gt 0 ]]
-then
-    if [[ "$1" == "deep" ]]
-    then
-        DEEPCLEAN=1
-    else
-        show_help
-    fi
-fi
+for arg in "$@"; do
+    case "$arg" in
+        --deep) DEEPCLEAN=1 ;;
+        -y) SKIP_CONFIRM=1 ;;
+        *) show_help ;;
+    esac
+done
 
 if [[ $DEEPCLEAN -eq 1 ]]
 then
@@ -33,22 +33,25 @@ then
 else
     echo "This will clean all uncommited changes, including UNTRACKED files."
 fi
-read ANSWER\?"Type 'yes' to proceed: "
 
-if [[ "$ANSWER" == 'yes' ]]
-then
-    echo "Cleaning repository ..."
-    cd $VSCODE_REPO
-    if [[ $DEEPCLEAN -eq 1 ]]
-    then
-        echo "Cleaning ..."
-        rm -rf $(ls -A | grep -v '^.git$')
-        echo "Restoring from commit ..."
-        git restore .
-    else
-        git reset --hard HEAD
-        git clean -fd
+if [[ $SKIP_CONFIRM -ne 1 ]]; then
+    read ANSWER\?"Type 'yes' to proceed: "
+    if [[ "$ANSWER" != 'yes' ]]; then
+        echo "Operation cancelled."
+        exit
     fi
+fi
+
+echo "Cleaning repository ..."
+cd $VSCODE_REPO
+git submodule deinit --all -f 2>/dev/null
+if [[ $DEEPCLEAN -eq 1 ]]
+then
+    echo "Cleaning ..."
+    rm -rf $(ls -A | grep -v '^.git$')
+    echo "Restoring from commit ..."
+    git restore .
 else
-    echo "Operation cancelled."
+    git reset --hard HEAD
+    git clean -fd
 fi
